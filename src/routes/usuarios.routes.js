49d3a817baa581d1f32fbba738ed4a42e32c720a
Miguel Angel Zambrano <angel245687@gmail.com>
2026-08-02 01:21:20 -0300

    add login en controllers

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import validacionUsuario from '../middlewares/usuarioValidacion.js'
-import  { listarUsuarios, buscarUsuario, crearUsuario, editarUsuario, usuarioPaginado, eliminarUsuario } from '../controllers/usuarios.controllers.js'
+import  { listarUsuarios, buscarUsuario, crearUsuario, editarUsuario, usuarioPaginado, eliminarUsuario, login } from '../controllers/usuarios.controllers.js'
 import usuarioValidacion from "../middlewares/usuarioValidacion.js";
 
 
 const router = Router()
 
 router.route('/').get(listarUsuarios).post(usuarioValidacion, crearUsuario)
+router.route('/login').post(login)
 router.route('/paginado').get(usuarioPaginado)
 router.route('/:id').get(buscarUsuario).put(usuarioValidacion, editarUsuario).delete(eliminarUsuario)
 
