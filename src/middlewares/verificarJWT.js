32b94adae81c57d7625ff344b7aa5290b13b5e01
Miguel Angel Zambrano <angel245687@gmail.com>
2026-08-02 14:58:24 -0300

    fix verificarJWT

--- src/middlewares/verificarJWT.js
+++ src/middlewares/verificarJWT.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
+import Usuario from '../models/usuario.js'
 
-
-const verificarJWT = (req, res, next) => {
+const verificarJWT = async(req, res, next) => {
     try {
         const authHeader = req.headers.authorization
         if(!authHeader || !authHeader.startsWith('Bearer ')){
@@ -9,6 +9,10 @@ const verificarJWT = (req, res, next) => {
         }
         const token = authHeader.split(' ')[1]
         const payload = jwt.verify(token, process.env.SECRETJWT)
+        const usuarioEncontrado = await Usuario.findById(payload.id)
+        if(!usuarioEncontrado){
+            return res.status(401).json({mensaje:"No existe un usuario válido"})
+        }
         req.idUsuario = payload.id 
         req.rolUsuario = payload.rol
         next()
