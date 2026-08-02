1d793881c3f0ceb2512c198388abe4b365fe0882
Miguel Angel Zambrano <angel245687@gmail.com>
2026-08-02 02:52:41 -0300

    fix usuarios.routes.js funciones no utilizadas

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import validacionUsuario from '../middlewares/usuarioValidacion.js'
-import  { listarUsuarios, buscarUsuario, crearUsuario, editarUsuario, usuarioPaginado, eliminarUsuario, login } from '../controllers/usuarios.controllers.js'
+import  {  crearUsuario, editarUsuario,  login } from '../controllers/usuarios.controllers.js'
 import usuarioValidacion from "../middlewares/usuarioValidacion.js";
 
 
 const router = Router()
 
-router.route('/').get(listarUsuarios).post(usuarioValidacion, crearUsuario)
+router.route('/').post(usuarioValidacion, crearUsuario)
 router.route('/login').post(login)
 //router.route('/paginado').get(usuarioPaginado)
-router.route('/:id').get(buscarUsuario).put(usuarioValidacion, editarUsuario)
+router.route('/:id').put(usuarioValidacion, editarUsuario)
 
 export default router
